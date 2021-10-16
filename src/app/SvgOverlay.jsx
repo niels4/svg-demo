a870431dfb88d35e770ac37719b50d4d956adf49
Niels Nielsen <niels.nielsen@gmail.com>
2021-10-15 18:30:39 -0600

    draw some simple rectangles over a large area

--- src/app/SvgOverlay.jsx
+++ src/app/SvgOverlay.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import {useWindowSize} from 'src/data/clientData.js'
+
+const colors = [
+  '#f00',
+  '#ff0',
+  '#55f',
+  '#0f0',
+  '#0ff'
+]
+let nextColorIndex = -1
+const nextColor = () => {
+  if (nextColorIndex >= colors.length - 1) { nextColorIndex = -1 }
+  nextColorIndex += 1
+  return colors[nextColorIndex]
+}
+
+const RoundedRect = ({x, y, height, width, color = nextColor()}) => {
+  return <rect x={x} y={y} width={width} height={height} stroke={color}
+    fill="none" strokeWidth="3" rx="8" />
+}
 
 const viewBoxString = (height, width) => `${0} ${0} ${width} ${height}`
 
 export const SvgOverlay = () => {
+  const svgRef = useRef()
   const {height, width} = useWindowSize()
+  useEffect(() => { nextColorIndex = -1 })
 
-  return <svg className='svg_overlay' viewBox={viewBoxString(height, width)} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
-    <circle cx={42} cy={75} r="10" fill="#f00" />
+  return <svg className='svg_overlay' ref={svgRef} viewBox={viewBoxString(height * 8, width * 8)} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+    <RoundedRect x={120} y={100} height={200} width={100} />
+    <RoundedRect x={120} y={400} height={200} width={800} />
+    <RoundedRect x={420} y={50} height={300} width={500} />
+    <RoundedRect x={1400} y={800} height={300} width={500} />
+    <RoundedRect x={2400} y={200} height={800} width={500} />
+    <RoundedRect x={200} y={1600} height={800} width={500} />
+    <RoundedRect x={4200} y={1600} height={800} width={500} />
+    <RoundedRect x={4000} y={120} height={800} width={500} />
+    <RoundedRect x={2000} y={1600} height={300} width={800} />
+    <RoundedRect x={2000} y={2000} height={300} width={800} />
+    <RoundedRect x={8000} y={4500} height={300} width={800} />
+    <RoundedRect x={6000} y={4000} height={900} width={800} />
+    <RoundedRect x={7200} y={4000} height={200} width={800} />
   </svg>
 }
