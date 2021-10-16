43175dba821edd0ad11456150628489a5e396c3e
Niels Nielsen <niels.nielsen@gmail.com>
2021-10-16 10:51:05 -0600

    maintain center while zooming in and out

--- src/controls/PanAndZoom.js
+++ src/controls/PanAndZoom.js
@@ -1,11 +1,11 @@
 import {useState, useEffect} from 'react'
 
 const ZOOM_SPEED = 0.1
-const MAX_ZOOM_IN = 0.2
+const MAX_ZOOM_IN = 1
 const MAX_ZOOM_OUT = 20
 
 const createViewBoxString = (height, width, panX = 0, panY = 0, zoom = 1) =>
-  `${parseInt(panX * zoom)} ${parseInt(panY * zoom)} ${parseInt(width * zoom)} ${parseInt(height * zoom)}`
+  `${panX} ${panY} ${width * zoom} ${height * zoom}`
 
 export const usePanAndZoom = (svgRef) => {
   const [viewBoxString, setViewBoxString] = useState("0 0 1 1")
@@ -28,21 +28,38 @@ export const usePanAndZoom = (svgRef) => {
     }
 
     const onZoom = (e) => {
+      const cx = panX + zoom * width / 2
+      const cy = panY + zoom * height / 2
+
       zoom += ZOOM_SPEED * e.deltaY
       if (zoom > MAX_ZOOM_OUT) {
         zoom = MAX_ZOOM_OUT
       } else if (zoom < MAX_ZOOM_IN) {
         zoom = MAX_ZOOM_IN
       }
+
+      // maintain center as we zoom in and out
+      const newCx = panX + zoom * width / 2
+      const newCy = panY + zoom * height / 2
+      panX += (cx - newCx)
+      panY += (cy - newCy)
+
       updateViewbox()
+
       e.preventDefault()
+    }
+
+    const onPanStart = (e) => {
+      console.log("clicked!", e)
     }
 
     window.addEventListener('resize', onResize)
     svg.addEventListener('wheel', onZoom)
+    svg.addEventListener('mousedown', onPanStart)
     return () => {
       window.removeEventListener('resize', onResize)
       svg.removeEventListener('wheel', onZoom)
+      svg.removeEventListener('mousedown', onPanStart)
     }
   }, [svgRef])
 
