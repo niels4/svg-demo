8ea52206c9f51ab6155cbcb456b59cdabb1d830f
Niels Nielsen <niels.nielsen@gmail.com>
2021-10-17 13:39:37 -0600

    have linear animation working

--- src/controls/animateZoomTo.js
+++ src/controls/animateZoomTo.js
@@ -1,4 +1,23 @@
+const DURATION = 1000
+
 export const animateZoomTo = (sPanX, sPanY, sZoom, ePanX, ePanY, eZoom, update) => {
-  console.log("animating zoom")
-  update(ePanX, ePanY, eZoom)
+  const start = Date.now()
+  const end = start + DURATION
+  const diffPanX = ePanX - sPanX
+  const diffPanY = ePanY - sPanY
+  const diffZoom = eZoom - sZoom
+  const animate = () => {
+    const now = Date.now()
+    if (now >= end) {
+      update(ePanX, ePanY, eZoom)
+      return
+    }
+    window.requestAnimationFrame(animate)
+    const percentComplete = (now - start) / DURATION
+    const newPanX = sPanX + percentComplete * diffPanX
+    const newPanY = sPanY + percentComplete * diffPanY
+    const newZoom = sZoom + percentComplete * diffZoom
+    update(newPanX, newPanY, newZoom)
+  }
+  animate()
 }
