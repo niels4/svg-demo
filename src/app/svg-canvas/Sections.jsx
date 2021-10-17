24925302fa0e2f960f9c2eac851a752ab9f9c355
Niels Nielsen <niels.nielsen@gmail.com>
2021-10-17 12:10:37 -0600

    wrap section elements in a group

--- src/app/svg-canvas/Sections.jsx
+++ src/app/svg-canvas/Sections.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const Section = ({x, y, height, width, name}) => <>
+export const Section = ({x, y, height, width, name}) => <g>
   <rect x={x} y={y} width={width} height={height} stroke='#eee'
     fill="none" strokeWidth="2" rx="8" strokeDasharray='10' />
   <text x={x + 10} y={y + 25} fontSize="22" fill="#bbb" fontFamily="maven-bold">{name}</text>
-</>
+</g>
 
 export const Sections = ({data}) => data.map(({x, y, height, width, name}) => <Section
   key={name} x={x} y={y} height={height} width={width} name={name} />)
